c8ba4c2a7d4c3d03d877841c28836523c1755545
diatomicDisaster <wilfrid.somogyi.15@ucl.ac.uk>
2021-05-14 21:11:01 +0100

    Sync minor changes

--- types.jl
+++ types.jl
@@ -1,30 +1,49 @@
+struct Potential
+    values   ::Vector{Float64}
+    lambda   ::Float64
+    ess      ::Float64
+    symmetry ::Bool
+    parity   ::Bool
+end
+
+Base.broadcastable(f::Potential) = Ref(f)
+
+struct Coupling
+    states::Tuple{Potential, Potential}
+    values::Vector{Float64}
+end
+
 abstract type AbstractBasis end
 
-struct Electronic{N<:Number, L<:Real} <: AbstractBasis
-    nstate::N 
-    lambda::L
+struct Electronic <: AbstractBasis
+    poten::Potential
+    ess::Float
+    sigma::Float64
+    lambda::Float64
 end
 
-struct Spin{T<:Real} <: AbstractBasis
-    ess::T
-    sigma::T
+struct Vibrational <: AbstractBasis
+    vee::Int
+    vec::Vector{Float64}
 end
 
-struct Vibrational{N<:Number, L<:Real} <: AbstractBasis
-    nstate::N
-    vee::Real
+struct Rotational <: AbstractBasis
+    jay::Float64
+    omega::Float64
+    em::Float64
 end
 
-struct Rotational{T<:Real} <: AbstractBasis
-    jay::T
-    omega::T
-end
-
-struct Rovibrational <: AbstractBasis
+struct Rovibronic <: AbstractBasis
     e :: Electronic
-    s :: Spin
     v :: Vibrational
     r :: Rotational
 end
 
-abstract type BasisSet end
+struct Hamiltonian
+    vibsep::Float64
+    vibgrid::Vector{Float64}
+    reducedmass::Float64
+    potentials::Tuple{Potential}
+    couplings::Tuple{Coupling}
+    matrix::Matrix{Float64}
+end
